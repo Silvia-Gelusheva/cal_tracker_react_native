e5b5b7016b590a66788e54e7cd5227e2e5dddc9a
Silvia-Gelusheva <100965569+Silvia-Gelusheva@users.noreply.github.com>
2026-01-17 16:26:32 +0200

    Update AddMeal.js
    Update AddMeal.js

--- components/adMeal/AddMeal.js
+++ components/adMeal/AddMeal.js
@@ -1,10 +1,11 @@
-import { Modal, Text } from 'react-native'
+import { Button, Modal, Text } from 'react-native'
 
 export default function AddMeal({ visible, onClose }) {
 
     return (
         <Modal visible={visible} onRequestClose={onClose}>
             <Text>Modal</Text>
+            <Button></Button>
         </Modal>
     )
 }
